779d5d3e855961922b93be1c5be86a96c20d2a4d
po jen su <brian71742@gmail.com>
2025-08-02 18:33:49 +0800

    debug(1)

--- src/Message.Infrastructure/Services/Messenger.cs
+++ src/Message.Infrastructure/Services/Messenger.cs
@@ -6,52 +6,30 @@ using System.Text.Encodings.Web;
 using System.Text.Json;
 using Message.Application.Models;
 using Message.Application.Services;
-using Message.Domain.Entities;
 using Message.Domain.Repositories;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Message.Infrastructure.Services;
-
-/// <summary>
-/// 代表一個使用者的長連接
-/// </summary>
-public class Connection
-{
-    public static Connection New(WebSocket socket, User user)
-    {
-        return new Connection()
-        {
-            Socket = socket,
-            User = user,
-        };
-    }
-    
-    /// <summary>
-    /// 長連接
-    /// </summary>
-    public required WebSocket Socket { get; set; }
-
-    /// <summary>
-    /// 使用者
-    /// </summary>
-    public required User User { get; set; }
-}
-
 
 internal class Messenger : IMessenger
 {
     //private readonly ConcurrentDictionary<long, WebSocket> _sockets;
+
+    private readonly ILogger<Messenger> _logger;
     private readonly ConcurrentDictionary<long, WebSocket> _sockets;
     
     
     private readonly IServiceScopeFactory _scopeFactory;
     
     public Messenger(
+        ILogger<Messenger> logger,
         IServiceScopeFactory scopeFactory)
     {
         _sockets = [];
         
         _scopeFactory = scopeFactory;
+        _logger = logger;
     }
     
     public async Task RunAsync(WebSocket socket, long userId)
@@ -69,35 +47,6 @@ internal class Messenger : IMessenger
 
         if (success)
         {
-            #region "TEST"
-            
-            var body    = Encoding.UTF8.GetBytes($"Hello: {userId}");
-
-            // processing - 向系統租用一塊記憶體空間
-            var pool = ArrayPool<byte>.Shared;
-            var buffer  = pool.Rent(body.Length);
-            
-            // processing - 將訊息放入剛剛租用的記憶體空間內
-            body.CopyTo(buffer,0);
-            var segment = new ArraySegment<byte>(buffer, 0, body.Length);
-            
-            // processing - 發送消息
-            if (socket.State == WebSocketState.Open)
-            {
-                await socket.SendAsync(
-                    segment, 
-                    WebSocketMessageType.Text, 
-                    endOfMessage: true,
-                    CancellationToken.None);
-            }
-            
-            // processing - 向系統歸還一塊記憶體空間
-            pool.Return(buffer);
-            
-            #endregion
-            
-            
-            
             // 跑邏輯
             await _runAsync(socket, userId);
         }
@@ -122,6 +71,9 @@ internal class Messenger : IMessenger
                     var content = Encoding.UTF8.GetString(buffer, 0, request.Count);
                     var message = JsonSerializer.Deserialize<MessageDto>(content);
                     message.From = userId;
+                    
+                    _logger.LogInformation($"收到信息：{content}");
+                    
                     await _sendMessageAsync(message);
                 }
                 // condition - 如果是 close 幀，就關閉 web socket
